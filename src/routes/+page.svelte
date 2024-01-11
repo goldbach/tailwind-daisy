d4d6127a4c2bd08418b71b5fb2669fc21e537edd
Jakob Goldbach <jakob.goldbach@gmail.com>
2024-01-11 22:33:07 +0100

    more daisy styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,38 @@
-<h1 class="text-3xl font-bold">Hello world!</h1>
+<h1 class="h1">Hello world!</h1>
 
-<div class="flex bg-gray-50 justify-between items-end">
-    <div class="basis-1/3">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
-        laborum. Quidem placeat doloribus excepturi explicabo? Eos id quasi
-        reprehenderit, accusantium quaerat adipisci, aliquam impedit dolor vel
-        molestiae nostrum! Quidem, rem? Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Dolore, laborum. Quidem placeat doloribus excepturi
-        explicabo? Eos id quasi reprehenderit, accusantium quaerat adipisci,
-        aliquam impedit dolor vel molestiae nostrum! Quidem, rem?
-    </div>
-    <div class="basis-1/3">
+<div class="border border-dashed p-8 *:mt-4 *:p-4 *:border">
+    <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
         laborum. Quidem placeat doloribus excepturi explicabo? Eos id quasi
         reprehenderit, accusantium quaerat adipisci, aliquam impedit dolor vel
         molestiae nostrum! Quidem, rem?
+    </p>
+
+    <div class="">
+        <button class="btn">Button</button>
+        <button class="btn btn-primary">Primary</button>
+        <button class="btn btn-primary btn-outline">Outline</button>
     </div>
-    <div class="basis-1/3">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
-        laborum. Quidem placeat doloribus excepturi explicabo? Eos id quasi
-        reprehenderit, accusantium quaerat adipisci, aliquam impedit dolor vel
-        molestiae nostrum! Quidem, rem?
+    <div>
+        <button class="btn btn-secondary">secondary</button>
+        <button class="btn btn-accent">accent</button>
+        <button class="btn btn-info">info</button>
+        <button class="btn btn-success">success</button>
+        <button class="btn btn-error">error</button>
+        <button class="btn btn-ghost">ghost</button>
+        <button class="btn btn-link">link</button>
     </div>
+
+    <form class="bg-base-200">
+        <label for="foo" class="label">Label</label>
+        <input type="text" class="input" id="foo">
+        <button class="btn btn-primary mt-6" type="submit">Submit</button>
+    </form>
+
 </div>
-
-<style lang="postcss">
-    :global(html) {
-        background-color: theme(colors.gray.400);
-    }
-</style>
+<div class="p-8 *:p-2 *:mt-2 *:border *:rounded *:h-24">
+    <div class="bg-neutral  text-neutral-content">neutral</div>
+    <div class="bg-base-100 text-base-content">base-100</div>
+    <div class="bg-base-200 text-base-content">base-200</div>
+    <div class="bg-base-300 text-base-content">base-300</div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,28 @@
+const colors = require('tailwindcss/colors')
+
 export default {
 
   content: ['./src/**/*.{html,js,svelte,ts}'],
 
-  // theme: {
-  //   extend: {}
-  // },
+  theme: {
+    extend: {}
+  },
 
-  // themes: [
-  //   {
-  //     mytheme: {
-  //       "primary": "#2563eb",
-  //       "secondary": "#7c3aed",
-  //       "accent": "#f97316",
-  //       "neutral": "#2a323c",
-  //       "base-100": "#f3f4f6",
-  //       "info": "#3abff8",
-  //       "success": "#36d399",
-  //       "warning": "#fbbd23",
-  //       "error": "#f87272",
-  //     },
-  //   }
-  // ],
-  plugins: [require("daisyui"), ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: colors.emerald["800"],
+          "primary-content": colors.white,
+          secondary: colors.amber["600"],
+          error: colors.rose["800"],
+          "error-content": colors.white,
+          "base-200": "#F7F7F7",
+        },
+      }
+    ],
+  },
+
+  plugins: [require("daisyui"),],
 };
