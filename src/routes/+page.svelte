d76be84d3233364ecf5100348788b4d749e30755
Jakob Goldbach <jakob.goldbach@gmail.com>
2024-01-11 20:41:16 +0100

    tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-3xl font-bold">Hello world!</h1>
+
+<div class="flex bg-gray-50 justify-between items-end">
+    <div class="basis-1/3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
+        laborum. Quidem placeat doloribus excepturi explicabo? Eos id quasi
+        reprehenderit, accusantium quaerat adipisci, aliquam impedit dolor vel
+        molestiae nostrum! Quidem, rem? Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Dolore, laborum. Quidem placeat doloribus excepturi
+        explicabo? Eos id quasi reprehenderit, accusantium quaerat adipisci,
+        aliquam impedit dolor vel molestiae nostrum! Quidem, rem?
+    </div>
+    <div class="basis-1/3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
+        laborum. Quidem placeat doloribus excepturi explicabo? Eos id quasi
+        reprehenderit, accusantium quaerat adipisci, aliquam impedit dolor vel
+        molestiae nostrum! Quidem, rem?
+    </div>
+    <div class="basis-1/3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
+        laborum. Quidem placeat doloribus excepturi explicabo? Eos id quasi
+        reprehenderit, accusantium quaerat adipisci, aliquam impedit dolor vel
+        molestiae nostrum! Quidem, rem?
+    </div>
+</div>
+
+<style lang="postcss">
+    :global(html) {
+        background-color: theme(colors.gray.400);
+    }
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+export default {
+
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+
+  theme: {
+    extend: {}
+  },
+
+  // themes: [
+  //   {
+  //     mytheme: {
+  //       "primary": "#2563eb",
+  //       "secondary": "#7c3aed",
+  //       "accent": "#f97316",
+  //       "neutral": "#2a323c",
+  //       "base-100": "#f3f4f6",
+  //       "info": "#3abff8",
+  //       "success": "#36d399",
+  //       "warning": "#fbbd23",
+  //       "error": "#f87272",
+  //     },
+  //   }
+  // ],
+  plugins: []
+};
